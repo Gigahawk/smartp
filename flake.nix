136561408d85efba30bb1cda1187edf7daeb68bd
Gigahawk <jasperchan515@gmail.com>
2026-05-27 00:38:34 -0700

    chore: migrate to uv2nix (#1)

--- flake.nix
+++ flake.nix
@@ -1,43 +1,177 @@
 {
-  description = "Application packaged using poetry2nix";
-
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs = {
+        pyproject-nix.follows = "pyproject-nix";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs = {
+        pyproject-nix.follows = "pyproject-nix";
+        uv2nix.follows = "uv2nix";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
+      treefmt-nix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+        inherit (nixpkgs) lib;
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python312;
+
+        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+        overlay = workspace.mkPyprojectOverlay {
+          sourcePreference = "wheel";
+        };
+        editableOverlay = workspace.mkEditablePyprojectOverlay {
+          root = "$REPO_ROOT";
+        };
+        hacks = pkgs.callPackage pyproject-nix.build.hacks { };
+
+        pyprojectOverrides = final: prev: {
+          pysmart = prev.pysmart.overrideAttrs (old: {
+            buildInputs = (old.buildInputs or [ ]) ++ [
+              prev.setuptools
+              prev.setuptools-scm
+              prev.vcs-versioning
+            ];
+          });
+          # Example overrides to fix build
+          # psycopg2 = prev.psycopg2.overrideAttrs (old: {
+          #   buildInputs = (old.buildInputs or [ ]) ++ [
+          #     prev.setuptools
+          #     pkgs.libpq.pg_config
+          #   ];
+          # });
+          # casadi = hacks.nixpkgsPrebuild {
+          #   from = pkgs.python312Packages.casadi;
+          #   prev = prev.casadi;
+          # };
+
+          ## TODO: Add tests to package?
+          ## Based on https://pyproject-nix.github.io/uv2nix/patterns/testing.html
+          ## Doesn't seem to work, smartp package isn't found
+          #smartp = prev.smartp.overrideAttrs (old: {
+          #  passthru = old.passthru // {
+          #    tests =
+          #      let
+          #        _virtualenv = final.mkVirtualEnv "smartp-pytest-env" workspace.deps.all // {
+          #          smartp = [ "dev" ];
+          #        };
+          #      in
+          #      (old.tests or { })
+          #      // {
+          #        pytest = pkgs.stdenv.mkDerivation {
+          #          name = "${final.smartp.name}-pytest";
+          #          inherit (final.smartp) src;
+          #          nativeBuildInputs = [
+          #            virtualenv
+          #            _virtualenv
+          #          ];
+          #          dontConfigure = true;
+          #          buildPhase = ''
+          #            runHook preBuild
+          #            pytest
+          #            runHook postBuild
+          #          '';
+          #        };
+          #      };
+          #  };
+          #});
+        };
+
+        pythonSet =
+          (pkgs.callPackage pyproject-nix.build.packages {
+            inherit python;
+          }).overrideScope
+            (
+              lib.composeManyExtensions [
+                pyproject-build-systems.overlays.wheel
+                overlay
+                pyprojectOverrides
+              ]
+            );
+
+        editablePythonSet = pythonSet.overrideScope editableOverlay;
+        virtualenv = editablePythonSet.mkVirtualEnv "smartp-dev-env" workspace.deps.all;
+
+        inherit (pkgs.callPackages pyproject-nix.build.util { }) mkApplication;
+
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
       in
       {
         packages = {
-          smartp = mkPoetryApplication {
-            projectDir = self;
-            nativeBuildInputs = [ pkgs.makeWrapper ];
-            propogatedBuildInputs = [ pkgs.smartmontools ];
-            postInstall = ''
-              wrapProgram "$out/bin/smartp" \
-                --prefix PATH : ${nixpkgs.lib.makeBinPath [ pkgs.smartmontools ]}
-            '';
-            # Dependency check is broken for git dependencies on current nixpkgs
-            dontCheckRuntimeDeps = true;
-          };
+          smartp =
+            (mkApplication {
+              venv = pythonSet.mkVirtualEnv "smartp-app-env" workspace.deps.default;
+              package = pythonSet.smartp;
+            }).overrideAttrs
+              (old: {
+                nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.makeWrapper ];
+                postInstall = ''
+                  wrapProgram "$out/bin/smartp" \
+                      --prefix PATH : ${nixpkgs.lib.makeBinPath [ pkgs.smartmontools ]}
+                '';
+              });
           default = self.packages.${system}.smartp;
         };
-
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.smartp ];
-          packages = [
-            pkgs.poetry
-            ];
+        formatter = treefmtEval.config.build.wrapper;
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+          # Doesn't seem to work
+          # pytest = editablePythonSet.smartp.passthru.tests.pytest;
         };
-      });
+        devShells = {
+          default = pkgs.mkShell {
+            packages = [
+              virtualenv
+              pkgs.uv
+              pkgs.sphinx
+              pkgs.git
+            ];
+            env = {
+              UV_NO_SYNC = "1";
+              UV_PYTHON = editablePythonSet.python.interpreter;
+              UV_PYTHON_DOWNLOADS = "never";
+            }
+            // lib.optionalAttrs pkgs.stdenv.isLinux {
+              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+            };
+            shellHook = ''
+              unset PYTHONPATH
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
+              . ${virtualenv}/bin/activate
+            '';
+          };
+        };
+      }
+    );
 }
---
+++ src/smartp/main.py
@@ -0,0 +1,86 @@
+import logging
+import concurrent.futures
+import argparse
+
+from pySMART import DeviceList, Device, TestEntry
+from .smarttester import SmartTester
+
+logger = logging.getLogger("smartp")
+
+
+def init_logging(debug: bool):
+    ch = logging.StreamHandler()
+    if debug:
+        logger.setLevel(logging.DEBUG)
+        ch.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+        ch.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    ch.setFormatter(formatter)
+    logger.handlers = [ch]
+    logger.debug("Initialized")
+
+
+def run_test(device: Device, test_type: str, debug: bool):
+    tester = SmartTester(device, test_type, debug=debug)
+    return tester.run_test()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="smartp",
+        description=(
+            "Run a smart test on all SMART capable drives attached to the system"
+        ),
+    )
+    parser.add_argument(
+        "-t",
+        "--test",
+        choices=["short", "long", "conveyance"],
+        default="short",
+        help="Type of test to run",
+    )
+    parser.add_argument(
+        "--threads", help="Number of tests to run simultaneously", default=20, type=int
+    )
+    parser.add_argument("--debug", help="Enable debug loggin", action="store_true")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    init_logging(args.debug)
+    devices = DeviceList()
+    if devices.devices:
+        logger.info(f"Running {args.test} SMART test on: {[d.name for d in devices]}")
+        failed_devices = []
+        with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as ex:
+            future_to_device = {
+                ex.submit(run_test, d, args.test, args.debug): d for d in devices
+            }
+            for future in concurrent.futures.as_completed(future_to_device):
+                device = future_to_device[future]
+                result = future.result()
+                if isinstance(result, TestEntry):
+                    logger.info(f"{device.name}: {result.status}")
+                    if result.status != "Completed without error":
+                        failed_devices.append(device)
+                        logger.error(
+                            "Found non-passed status for:\n"
+                            f"- device: {device}\n"
+                            f"- result: {result}"
+                        )
+                elif isinstance(result, str):
+                    logger.warning(f"{device.name}: {result}")
+                    if f"Device {device.name} does not support the " in result:
+                        continue
+        logger.info("Tests finished")
+        if failed_devices:
+            logger.error(f"Failed devices: {[d.name for d in failed_devices]}")
+        exit(len(failed_devices))
+    else:
+        logger.error("No SMART capable disks found")
+        exit(0)
